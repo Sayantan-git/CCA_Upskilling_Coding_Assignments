e4f34a9e01f78b3e2a1248eaed3e59b661e29a39
Sayantan <sayantanb2000@gmail.com>
2022-11-11 15:56:02 +0530

    Assignment 4

---
+++ Assignment3.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Assignment3._3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch;
+            StackException se = new StackException();
+            do
+            {
+                Console.WriteLine("\n1.Push\n2.Pop\n3.Exit");
+                Console.WriteLine("\nEnter the operation");
+                ch = Convert.ToInt32(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the value");
+                        int data = Convert.ToInt32(Console.ReadLine());
+                        se.Push(data);
+                        break;
+                    case 2:
+                        se.Pop();
+                        break;
+                    case 3: break;
+                }
+            }
+            while (ch != 3);
+        }
+    }
+}
+
---
+++ Assignment3.3/StackException.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3._3
+{
+public class StackException : Exception
+    {
+        public StackException() { }
+        public StackException(string message) : base(String.Format(message))
+        {
+        }
+
+        static int MAX = 5;
+        int[] st = new int[MAX];
+        int top = -1;
+        bool isempty()
+        {
+            if (top == -1)
+                return true;
+            else
+                return false;
+        }
+        public void Push(int data)
+        {
+            try
+            {
+                if (top > MAX)
+                {
+                    throw new StackException("Stack Overflow");
+                }
+                else
+                {
+                    st[++top] = data;
+                    top++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void Pop()
+        {
+            int data;
+            try
+            {
+                if (!isempty())
+                {
+                    data = st[top];
+                    top = top - 1;
+                }
+                else
+                {
+                    throw new StackException("Stack Underflow");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Stack Underflow");
+            }
+        }
+    }
+}
